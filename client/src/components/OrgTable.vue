21260d416d2e5555f9b523ee8213879832dbee51
Bruno Carvalho <bruno.js.carvalho123@gmail.com>
2021-01-13 19:53:15 +0000

    New element Table + fix styling

---
+++ client/src/components/OrgTable.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-simple-table fixed-header height="300px">
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <template v-for="header in headers">
+            <th v-if="header.leftCorner" :key="header.name" class='table-header header-left'>
+              {{header.name}}
+              <!-- <v-icon class="sort-icon">mdi-arrow-up-down-bold</v-icon> -->
+            </th>
+            <th v-if="header.rightCorner" :key="header.name" class='table-header header-right'>
+              {{header.name}}
+              <!-- <v-icon class="sort-icon">mdi-arrow-up-down-bold</v-icon> -->
+            </th>
+            <th v-if="header.single" :key="header.name" class='table-header header-single'>
+              {{header.name}}
+              <!-- <v-icon class="sort-icon">mdi-arrow-up-down-bold</v-icon> -->
+            </th>
+            <th v-if="header.middle" :key="header.name" class='table-header'>
+              {{header.name}}
+              <!-- <v-icon class="sort-icon">mdi-arrow-up-down-bold</v-icon> -->
+            </th>
+          </template>
+        </tr>
+      </thead>
+      <tbody>
+          <tr v-for="item in items" v-bind:key="item.id">
+            <template v-for="prop in Object.keys(item)">
+              <td  v-bind:key="prop+item.id" v-if="prop!='id'" class="table-row">{{ item[prop] }}</td>
+            </template>
+          </tr>
+          <tr v-if="addRow">
+            <td class="add-row" :colspan="headers.length">
+              <v-icon color="var(--org-blue)">
+                mdi-plus-circle
+              </v-icon>
+            </td>
+          </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<style scoped>
+  .table-header {
+    background-color: var(--org-blue) !important;
+    color: white !important;
+    font-size: 11pt !important;
+  }
+
+  .header-left {
+    border-top-left-radius: 10px;
+  }
+
+  .header-right {
+    border-top-right-radius: 10px;
+  }
+
+  .header-single {
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+  }
+
+  .table-row {
+    font-size: 10pt !important;
+  }
+
+  .add-row {
+    text-align: center;
+    border-bottom: 1px solid var(--org-nice-grey);
+  }
+
+  tbody tr:nth-of-type(even) {
+    background-color: rgba(0, 0, 0, .05);
+  }
+
+  .sort-icon {
+    font-size: 11pt;
+    color: var(--org-light-blue)
+  }
+</style>
+
+<script>
+  export default {
+    name: 'OrgTable',
+    props: ['headers','items', 'addRow'],
+    data: () => ({
+    }),
+  }
+</script>
--- client/src/views/Area.vue
+++ client/src/views/Area.vue
@@ -1,25 +1,10 @@
 <template>
   <v-container class="main-container" fluid>
     <v-text-field class="area-name" label="Área" v-bind:value=area.name></v-text-field>
-
-    <v-simple-table fixed-header height="300px">
-      <template v-slot:default>
-        <thead>
-          <tr class="table-header">
-            <th class="text-left">Ticket</th>
-            <th class="text-left">URL</th>
-            <th class="text-left">Prioridade</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="ticket in area.tickets" :key="ticket.id">
-            <td>{{ ticket.name }}</td>
-            <td>{{ ticket.url }}</td>
-            <td>{{ ticket.priority }}</td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
+    <OrgTable class="ticket-table" :headers="area.tickets.headers" :items="area.tickets.rows" add-row="true"/>
+    <OrgTable class="process-table" :headers="area.processes.headers" :items="area.processes.rows" add-row="true"/>
+    <OrgTable class="employee-table" :headers="area.employees.headers" :items="area.employees.rows" add-row="true"/>
+    <OrgTable class="project-table" :headers="area.projects.headers" :items="area.projects.rows" add-row="true"/>
   </v-container>
 </template>
 
@@ -34,34 +19,80 @@
     width: 16%;
   }
 
-  tbody tr:nth-of-type(even) {
-    background-color: rgba(0, 0, 0, .05);
-  }
-
-  .v-label{
+  .v-label {
     font-size: 70px !important;
   }
 
-  .table-header {
-    background-color: var(--org-blue) !important;
+  .ticket-table {
+    width: 45%;
+  }
+
+  .employee-table {
+    width: 15%;
+  }
+
+  .process-table {
+    width: 25%;
   }
 </style>
 
 <script>
-  export default {
-    name: 'Area',
+  import OrgTable from '../components/OrgTable';
 
+  export default {
+    components: { OrgTable },
+    name: 'Area',
     data: () => ({
       area: {
         name: 'Área 1',
-        tickets: [
-          {id: 1, name: 'ticket teste 1', url: 'www.ticket1.com', priority: 'Alta'},
-          {id: 2, name: 'ticket teste 2', url: 'www.ticket2.com', priority: 'Media'},
-          {id: 3, name: 'ticket teste 3', url: 'www.ticket3.com', priority: 'Baixa'}
-        ],
-        processes: [],
-        employees: [],
-        project: []
+        tickets: {
+          headers: [
+            {leftCorner: true, name: 'Ticket'},
+            {middle: true, name: 'URL'},
+            {rightCorner: true, name: 'Prioridade'}
+          ],
+          rows: [
+            {id: 1, name: 'ticket teste 1', url: 'www.ticket1.com', priority: 'Alta'},
+            {id: 2, name: 'ticket teste 2', url: 'www.ticket2.com', priority: 'Media'},
+            {id: 3, name: 'ticket teste 3', url: 'www.ticket3.com', priority: 'Baixa'},
+            {id: 4, name: 'ticket teste 3', url: 'www.ticket3.com', priority: 'Baixa'}
+          ]
+        },
+        processes: {
+          headers: [
+            {leftCorner: true, name: 'Processo'},
+            {rightCorner: true, name: 'Nº erros na última hora'}
+          ],
+          rows: [
+            {id: 1, name: 'Processo 1', nErrors: 5},
+            {id: 2, name: 'Processo 2', nErrors: 5},
+            {id: 3, name: 'Processo 3', nErrors: 5},
+            {id: 4, name: 'Processo 4', nErrors: 5}
+          ]
+        },
+        employees: {
+          headers: [
+            {single: true, name: 'Responsável'}
+          ],
+          rows: [
+            {id: 1, name: 'João blahblah'},
+            {id: 2, name: 'João blahblasd2'},
+            {id: 3, name: 'João blahblah 3'}
+          ]
+        },
+        projects: {
+          headers: [
+            {leftCorner: true, name: 'Projeto'},
+            {middle: true, name: 'Progresso'},
+            {rightCorner: true, name: 'Dias restantes'}
+          ],
+          rows: [
+            {id: 1, name: 'Projeto da area 1', progress: 25, daysLeft: 93},
+            {id: 2, name: 'Projeto da area 2', progress: 75, daysLeft: 35},
+            {id: 3, name: 'Projeto da area 3', progress: 50, daysLeft: 103},
+            {id: 4, name: 'Projeto da area 3', progress: 90, daysLeft: 12}
+          ]
+        }
       }
     }),
   }
